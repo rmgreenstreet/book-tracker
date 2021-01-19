95f00e2990f55ebf5163a6dace26b7cbff549e51
Robert Greenstreet <robertgreenstreet@gmail.com>
2021-01-19 17:46:43 -0600

    Begin layout for home page and 'books i've read'

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-const siteTitle = " - Meenen Transmission | Emporia, KS Mechanic | 800-903-8855"
+const siteTitle = " - Book Tracker | What Should I Read Next?"
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home' + siteTitle });
 });
 
-/* GET recipes page. */
-router.get('/recipes', function(req, res, next) {
-  res.render('recipes', { title: 'Recipes' });
+/* GET books-read page. */
+router.get('/books-read', function(req, res, next) {
+  res.render('books-read', { title: 'Books I\'ve Read' + siteTitle });
 });
 
-/* GET ingredients page. */
-router.get('/ingredients', function(req, res, next) {
-  res.render('ingredients', { title: 'Ingredients' });
-});
-
-/* GET plans page. */
-router.get('/plans', function(req, res, next) {
-  res.render('plans', { title: 'Meal Plans' });
-});
-
-/* GET shopping-list page. */
-router.get('/shopping-list', function(req, res, next) {
-  res.render('shopping-list', { title: 'Shopping List' });
+/* GET browse-books page. */
+router.get('/browse-books', function(req, res, next) {
+  res.render('browse-books', { title: 'Browse Books' + siteTitle });
 });
 
 module.exports = router;
