8745bd5210a3dae5bf80562965675827c17b1902
Robert Greenstreet <robertgreenstreet@gmail.com>
2021-02-03 15:46:48 -0600

    Further styling for tag cloud on review details

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,9 @@
 window.onload = function () {
     formatTagCloud();
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 };
 
 function formatTagCloud() {
